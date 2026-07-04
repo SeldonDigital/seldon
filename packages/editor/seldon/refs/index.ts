9aa3d91b9807ea825821a71818748d800be94d98
SeldonDigital <andrei@seldon.digital>
2026-07-04 02:47:13 +0200

    run format

--- packages/editor/seldon/refs/index.ts
+++ packages/editor/seldon/refs/index.ts
@@ -4,13 +4,13 @@
  *
  * License: https://github.com/SeldonDigital/seldon/blob/main/LICENSE.md
  * Do not redistribute or sublicense without permission.
- * 
- * You may not use this software, or any derivative works of it, in whole or in part, 
- * for the purposes of training, fine-tuning, or otherwise improving (directly or indirectly) 
+ *
+ * You may not use this software, or any derivative works of it, in whole or in part,
+ * for the purposes of training, fine-tuning, or otherwise improving (directly or indirectly)
  * any machine learning or artificial intelligence system without written permission.
- * 
+ *
  *****/
- 
+
 export type SeldonRef =
   | "filterActions"
   | "filterIcon"
@@ -60,44 +60,204 @@ export interface SeldonRefEntry {
 }
 
 export const SELDON_REFS: Record<SeldonRef, SeldonRefEntry> = {
-  "filterActions": {"component":"ButtonIconic","nodeId":"component-comboboxField-egV44OiP","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
-  "filterIcon": {"component":"Icon","nodeId":"component-comboboxField-ta9b5fTa","className":"sdn-icon sdn-icon--xi68"},
-  "filterLabel": {"component":"Input","nodeId":"component-comboboxField-TWyxOQad","className":"sdn-input sdn-input--twyx"},
-  "logo": {"component":"Frame","nodeId":"component-frame-AjNqkrLb","className":"sdn-frame sdn-frame--ajnq"},
-  "menuComponent": {"component":"ButtonSimple","nodeId":"component-bar-tJLcDfnC","className":"sdn-button-simple sdn-button-simple--dbgs"},
-  "menuDev": {"component":"ButtonSimple","nodeId":"component-bar-EW9LJxSD","className":"sdn-button-simple sdn-button-simple--dbgs"},
-  "menuEdit": {"component":"ButtonSimple","nodeId":"component-bar-8iQ7zRar","className":"sdn-button-simple sdn-button-simple--dbgs"},
-  "menuFile": {"component":"ButtonSimple","nodeId":"component-bar-dBgSvhzY","className":"sdn-button-simple sdn-button-simple--dbgs"},
-  "menuMode": {"component":"ButtonMenu","nodeId":"component-bar-JXHsaaaQ","className":"sdn-button-menu sdn-button-iconic--pgsr"},
-  "menus": {"component":"Frame","nodeId":"component-bar-DrSavE9B","className":"sdn-frame sdn-frame--drsa"},
-  "menuStates": {"component":"ButtonMenu","nodeId":"component-button-OTbyoxbN","className":"sdn-button-menu sdn-button"},
-  "menuTheme": {"component":"ButtonMenu","nodeId":"component-button-uIofK8RQ","className":"sdn-button-menu sdn-button-iconic--pgsr"},
-  "menuView": {"component":"ButtonSimple","nodeId":"component-bar-FUwSPfCT","className":"sdn-button-simple sdn-button-simple--dbgs"},
-  "nodeActions": {"component":"ButtonIconic","nodeId":"component-item-CeZRPCDC","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
-  "nodeIcon": {"component":"Icon","nodeId":"component-item-zdDEhaL4","className":"sdn-icon sdn-icon--xi68"},
-  "nodeLabel": {"component":"Input","nodeId":"component-item-pZCfJ3k6","className":"sdn-input sdn-input--pzcf"},
-  "nodeToggle": {"component":"ButtonIconic","nodeId":"component-item-HSgjhz6b","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
-  "nodeToggleIcon": {"component":"Icon","nodeId":"component-item-zn8GFZsT","className":"sdn-icon sdn-icon--vsau"},
-  "objectsContainer": {"component":"Frame","nodeId":"component-sidebar-ENPyLuzb","className":"sdn-frame sdn-frame--enpy"},
-  "optionIcon": {"component":"Icon","nodeId":"component-icon-3QOuNmn2","className":"sdn-icon sdn-icon--3qou"},
-  "optionLabel": {"component":"TextLabel","nodeId":"component-text-xOhbdtNu","className":"sdn-text-label sdn-text-label--xohb"},
-  "projectActions": {"component":"ButtonIconic","nodeId":"component-comboboxField-Td9lePEX","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
-  "projectIcon": {"component":"Icon","nodeId":"component-comboboxField-h6DYE6Jl","className":"sdn-icon sdn-icon--xi68"},
-  "projectLabel": {"component":"Input","nodeId":"component-comboboxField-Umc9UbAs","className":"sdn-input sdn-input--twyx"},
-  "propertiesContainer": {"component":"Frame","nodeId":"component-sidebar-evMwxVOP","className":"sdn-frame sdn-frame--enpy"},
-  "propertyActions": {"component":"ButtonIconic","nodeId":"component-button-CGRbb6mm","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
-  "propertyLabel": {"component":"Input","nodeId":"component-item-JvSW6JpE","className":"sdn-input sdn-input--jvsw"},
-  "propertyToggle": {"component":"ButtonIconic","nodeId":"component-button-iVVLVSBT","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
-  "propertyToggleIcon": {"component":"Icon","nodeId":"component-icon-Aa4AD1wO","className":"sdn-icon sdn-icon--vsau"},
-  "searchActions": {"component":"ButtonIconic","nodeId":"component-button-q7tCLRdW","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
-  "searchIcon": {"component":"Icon","nodeId":"component-icon-CHSbwIMc","className":"sdn-icon sdn-icon--xi68"},
-  "searchLabel": {"component":"Input","nodeId":"component-input-w7F4idVF","className":"sdn-input sdn-input--yoqi"},
-  "sectionActions": {"component":"ButtonIconic","nodeId":"component-item-m1G2OAIO","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
-  "sectionAdd": {"component":"ButtonIconic","nodeId":"component-item-sDjvfAPl","className":"sdn-button-iconic sdn-button-iconic--sdjv"},
-  "sectionLabel": {"component":"TextLabel","nodeId":"component-item-Z34z7Dhr","className":"sdn-text-label sdn-text-label--z34z"},
-  "sectionToggle": {"component":"ButtonIconic","nodeId":"component-item-OCtkZUuF","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
-  "sectionToggleIcon": {"component":"Icon","nodeId":"component-item-7MKLAjub","className":"sdn-icon sdn-icon--umgs"},
-  "valueIcon": {"component":"Icon","nodeId":"component-icon-V1g4W5fN","className":"sdn-icon sdn-icon--xi68"},
-  "valueLabel": {"component":"Input","nodeId":"component-input-IeGTgo7S","className":"sdn-input sdn-input--iegt"},
-  "valueOptionsMenu": {"component":"ButtonIconic","nodeId":"component-button-HqmnST2I","className":"sdn-button-iconic sdn-button-iconic--pgsr"},
+  filterActions: {
+    component: "ButtonIconic",
+    nodeId: "component-comboboxField-egV44OiP",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
+  filterIcon: {
+    component: "Icon",
+    nodeId: "component-comboboxField-ta9b5fTa",
+    className: "sdn-icon sdn-icon--xi68",
+  },
+  filterLabel: {
+    component: "Input",
+    nodeId: "component-comboboxField-TWyxOQad",
+    className: "sdn-input sdn-input--twyx",
+  },
+  logo: {
+    component: "Frame",
+    nodeId: "component-frame-AjNqkrLb",
+    className: "sdn-frame sdn-frame--ajnq",
+  },
+  menuComponent: {
+    component: "ButtonSimple",
+    nodeId: "component-bar-tJLcDfnC",
+    className: "sdn-button-simple sdn-button-simple--dbgs",
+  },
+  menuDev: {
+    component: "ButtonSimple",
+    nodeId: "component-bar-EW9LJxSD",
+    className: "sdn-button-simple sdn-button-simple--dbgs",
+  },
+  menuEdit: {
+    component: "ButtonSimple",
+    nodeId: "component-bar-8iQ7zRar",
+    className: "sdn-button-simple sdn-button-simple--dbgs",
+  },
+  menuFile: {
+    component: "ButtonSimple",
+    nodeId: "component-bar-dBgSvhzY",
+    className: "sdn-button-simple sdn-button-simple--dbgs",
+  },
+  menuMode: {
+    component: "ButtonMenu",
+    nodeId: "component-bar-JXHsaaaQ",
+    className: "sdn-button-menu sdn-button-iconic--pgsr",
+  },
+  menus: {
+    component: "Frame",
+    nodeId: "component-bar-DrSavE9B",
+    className: "sdn-frame sdn-frame--drsa",
+  },
+  menuStates: {
+    component: "ButtonMenu",
+    nodeId: "component-button-OTbyoxbN",
+    className: "sdn-button-menu sdn-button",
+  },
+  menuTheme: {
+    component: "ButtonMenu",
+    nodeId: "component-button-uIofK8RQ",
+    className: "sdn-button-menu sdn-button-iconic--pgsr",
+  },
+  menuView: {
+    component: "ButtonSimple",
+    nodeId: "component-bar-FUwSPfCT",
+    className: "sdn-button-simple sdn-button-simple--dbgs",
+  },
+  nodeActions: {
+    component: "ButtonIconic",
+    nodeId: "component-item-CeZRPCDC",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
+  nodeIcon: {
+    component: "Icon",
+    nodeId: "component-item-zdDEhaL4",
+    className: "sdn-icon sdn-icon--xi68",
+  },
+  nodeLabel: {
+    component: "Input",
+    nodeId: "component-item-pZCfJ3k6",
+    className: "sdn-input sdn-input--pzcf",
+  },
+  nodeToggle: {
+    component: "ButtonIconic",
+    nodeId: "component-item-HSgjhz6b",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
+  nodeToggleIcon: {
+    component: "Icon",
+    nodeId: "component-item-zn8GFZsT",
+    className: "sdn-icon sdn-icon--vsau",
+  },
+  objectsContainer: {
+    component: "Frame",
+    nodeId: "component-sidebar-ENPyLuzb",
+    className: "sdn-frame sdn-frame--enpy",
+  },
+  optionIcon: {
+    component: "Icon",
+    nodeId: "component-icon-3QOuNmn2",
+    className: "sdn-icon sdn-icon--3qou",
+  },
+  optionLabel: {
+    component: "TextLabel",
+    nodeId: "component-text-xOhbdtNu",
+    className: "sdn-text-label sdn-text-label--xohb",
+  },
+  projectActions: {
+    component: "ButtonIconic",
+    nodeId: "component-comboboxField-Td9lePEX",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
+  projectIcon: {
+    component: "Icon",
+    nodeId: "component-comboboxField-h6DYE6Jl",
+    className: "sdn-icon sdn-icon--xi68",
+  },
+  projectLabel: {
+    component: "Input",
+    nodeId: "component-comboboxField-Umc9UbAs",
+    className: "sdn-input sdn-input--twyx",
+  },
+  propertiesContainer: {
+    component: "Frame",
+    nodeId: "component-sidebar-evMwxVOP",
+    className: "sdn-frame sdn-frame--enpy",
+  },
+  propertyActions: {
+    component: "ButtonIconic",
+    nodeId: "component-button-CGRbb6mm",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
+  propertyLabel: {
+    component: "Input",
+    nodeId: "component-item-JvSW6JpE",
+    className: "sdn-input sdn-input--jvsw",
+  },
+  propertyToggle: {
+    component: "ButtonIconic",
+    nodeId: "component-button-iVVLVSBT",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
+  propertyToggleIcon: {
+    component: "Icon",
+    nodeId: "component-icon-Aa4AD1wO",
+    className: "sdn-icon sdn-icon--vsau",
+  },
+  searchActions: {
+    component: "ButtonIconic",
+    nodeId: "component-button-q7tCLRdW",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
+  searchIcon: {
+    component: "Icon",
+    nodeId: "component-icon-CHSbwIMc",
+    className: "sdn-icon sdn-icon--xi68",
+  },
+  searchLabel: {
+    component: "Input",
+    nodeId: "component-input-w7F4idVF",
+    className: "sdn-input sdn-input--yoqi",
+  },
+  sectionActions: {
+    component: "ButtonIconic",
+    nodeId: "component-item-m1G2OAIO",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
+  sectionAdd: {
+    component: "ButtonIconic",
+    nodeId: "component-item-sDjvfAPl",
+    className: "sdn-button-iconic sdn-button-iconic--sdjv",
+  },
+  sectionLabel: {
+    component: "TextLabel",
+    nodeId: "component-item-Z34z7Dhr",
+    className: "sdn-text-label sdn-text-label--z34z",
+  },
+  sectionToggle: {
+    component: "ButtonIconic",
+    nodeId: "component-item-OCtkZUuF",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
+  sectionToggleIcon: {
+    component: "Icon",
+    nodeId: "component-item-7MKLAjub",
+    className: "sdn-icon sdn-icon--umgs",
+  },
+  valueIcon: {
+    component: "Icon",
+    nodeId: "component-icon-V1g4W5fN",
+    className: "sdn-icon sdn-icon--xi68",
+  },
+  valueLabel: {
+    component: "Input",
+    nodeId: "component-input-IeGTgo7S",
+    className: "sdn-input sdn-input--iegt",
+  },
+  valueOptionsMenu: {
+    component: "ButtonIconic",
+    nodeId: "component-button-HqmnST2I",
+    className: "sdn-button-iconic sdn-button-iconic--pgsr",
+  },
 }
